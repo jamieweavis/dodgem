a5f52a0dc72635486821c6df37ed2bfecca37f46
Jamie Weavis <jamie.weavis@gmail.com>
2018-04-30 20:48:04 +0100

    Switch from standard to airbnb with eslint & prettier

--- src/Dodgem.js
+++ src/Dodgem.js
@@ -1,168 +1,199 @@
-'use strict'
-
-const Ora = require('ora')
-const chalk = require('chalk')
-const moment = require('moment')
-const inquirer = require('inquirer')
-const puppeteer = require('puppeteer')
+const Ora = require('ora');
+const chalk = require('chalk');
+const moment = require('moment');
+const inquirer = require('inquirer');
+const puppeteer = require('puppeteer');
 
 class Dodgem {
-
   /**
    * Stores saved credentials against the instance
    *
    * @param {Object} credentials
    */
-  constructor (credentials) {
-    this.credentials = credentials
-    this.interval = null
-    this.target = null
-    this.page = null
+  constructor(credentials) {
+    this.credentials = credentials;
+    this.interval = null;
+    this.target = null;
+    this.page = null;
   }
 
   /**
    * Prompt the user for bump target & interval then initialize Puppeteer browser & page
    */
-  async init () {
+  async init() {
     // Bump target & interval prompt
-    await inquirer.prompt([{
-      type: 'list',
-      name: 'target',
-      message: 'Bump target:',
-      choices: [
-        { name: 'All trades', value: 'all' },
-        { name: 'Oldest trade', value: 'oldest' }
-      ]
-    }, {
-      name: 'interval',
-      message: 'Repeat Interval (minutes):',
-      validate: (input) => {
-        if (!input || !input.match(/^\d{0,2}(\.\d{1,2})?$/)) {
-          return 'Please enter a valid number'
+    await inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'target',
+          message: 'Bump target:',
+          choices: [
+            { name: 'All trades', value: 'all' },
+            { name: 'Oldest trade', value: 'oldest' }
+          ]
+        },
+        {
+          name: 'interval',
+          message: 'Repeat Interval (minutes):',
+          validate: input => {
+            if (!input || !input.match(/^\d{0,2}(\.\d{1,2})?$/)) {
+              return 'Please enter a valid number';
+            }
+            return true;
+          }
         }
-        return true
-      }
-    }]).then(async data => {
-      this.target = data.target
-      this.interval = data.interval
-    })
+      ])
+      .then(async data => {
+        this.target = data.target;
+        this.interval = data.interval;
+      });
 
     // Launch Puppeteer
-    const browser = await puppeteer.launch()
-    this.page = await browser.newPage()
+    const browser = await puppeteer.launch();
+    this.page = await browser.newPage();
 
     // Spinner
-    const target = this.target === 'oldest' ? 'the oldest trade' : 'all trades'
-    new Ora(`Bumping ${chalk.blue(target)} every ${chalk.blue(this.interval)} minutes`).info()
+    const target = this.target === 'oldest' ? 'the oldest trade' : 'all trades';
+    new Ora(
+      `Bumping ${chalk.blue(target)} every ${chalk.blue(this.interval)} minutes`
+    ).info();
   }
 
   /**
    * Log in to Rocket League Garage with stored credentials
    */
-  async login () {
+  async login() {
     // Spinner
     const spinner = new Ora({
       text: `Logging in as: ${chalk.blue(this.credentials.emailAddress)}`,
       color: 'yellow'
-    }).start()
+    }).start();
 
     try {
       // Login page
-      await this.page.goto('https://rocket-league.com/login')
+      await this.page.goto('https://rocket-league.com/login');
 
       // Email Address
-      await this.page.focus('.rlg-form .rlg-input[type="email"]')
-      await this.page.type(this.credentials.emailAddress)
+      await this.page.focus('.rlg-form .rlg-input[type="email"]');
+      await this.page.type(this.credentials.emailAddress);
 
       // Password
-      await this.page.focus('.rlg-form .rlg-input[type="password"]')
-      await this.page.type(this.credentials.password)
+      await this.page.focus('.rlg-form .rlg-input[type="password"]');
+      await this.page.type(this.credentials.password);
 
       // Submit
-      await this.page.click('.rlg-form .rlg-btn-primary[type="submit"]')
-      await this.page.waitForSelector('.rlg-header-main-welcome-user')
+      await this.page.click('.rlg-form .rlg-btn-primary[type="submit"]');
+      await this.page.waitForSelector('.rlg-header-main-welcome-user');
 
-      spinner.succeed(`Logged in as: ${chalk.blue(this.credentials.emailAddress)}`)
+      spinner.succeed(
+        `Logged in as: ${chalk.blue(this.credentials.emailAddress)}`
+      );
     } catch (error) {
-      spinner.fail(`Failed to login in as: ${chalk.blue(this.credentials.emailAddress)}`)
+      spinner.fail(
+        `Failed to login in as: ${chalk.blue(this.credentials.emailAddress)}`
+      );
       throw new Error(
-        chalk.red('Login failed - please ensure login details are correct. Run `dodgem login` to update login details.')
-      )
+        chalk.red(
+          'Login failed - please ensure login details are correct. Run `dodgem login` to update login details.'
+        )
+      );
     }
   }
 
   /**
    * Scrape active trade offers
    */
-  async scrapeTrades () {
+  async scrapeTrades() {
     // Spinner
     const spinner = new Ora({
       text: 'Finding active trades',
       color: 'yellow'
-    }).start()
+    }).start();
 
     // Navigate to active trades
-    await this.page.goto('https://rocket-league.com/trading')
-    await this.page.hover('.rlg-header-main-welcome-user')
-    await this.page.click("[href^='/trades']")
-    await this.page.waitForNavigation()
+    await this.page.goto('https://rocket-league.com/trading');
+    await this.page.hover('.rlg-header-main-welcome-user');
+    await this.page.click("[href^='/trades']");
+    await this.page.waitForNavigation();
 
     // Scrape trade URLs
     let tradeUrls = await this.page.evaluate(() => {
-      const anchors = Array.from(document.querySelectorAll('.rlg-trade-display-header > a'))
-      return anchors.map(anchor => anchor.href)
-    })
-    spinner.succeed(`Found ${chalk.blue(tradeUrls.length)} active trade${tradeUrls.length === 1 ? '' : 's'}`)
+      const anchors = Array.from(
+        document.querySelectorAll('.rlg-trade-display-header > a')
+      );
+      return anchors.map(anchor => anchor.href);
+    });
+    spinner.succeed(
+      `Found ${chalk.blue(tradeUrls.length)} active trade${
+        tradeUrls.length === 1 ? '' : 's'
+      }`
+    );
 
     // @TODO: Filter out trades that are not editable due to 15 minute cooldown period
 
-    if (this.target === 'oldest') tradeUrls = [tradeUrls[tradeUrls.length - 1]]
-    return tradeUrls
+    if (this.target === 'oldest') tradeUrls = [tradeUrls[tradeUrls.length - 1]];
+    return tradeUrls;
   }
 
   /**
-   * Bump specified trades
+   * Bump array of trade URLs
    *
    * @param {Object[]} tradeUrls
    */
-  async bumpTrades (tradeUrls) {
+  async bumpTrades(tradeUrls) {
+    /* eslint-disable */
+    // TODO: Rethink this method if we want eslint to be happy about it
     for (let [index, tradeUrl] of tradeUrls.entries()) {
-      const humanIndex = index + 1
-      const start = moment()
+      const humanIndex = index + 1;
+      const start = moment();
 
       const spinner = new Ora({
-        text: this.target === 'oldest' ? 'Bumping oldest active trade' : `Bumping trade ${humanIndex}/${tradeUrls.length}`,
+        text:
+          this.target === 'oldest'
+            ? 'Bumping oldest active trade'
+            : `Bumping trade ${humanIndex}/${tradeUrls.length}`,
         color: 'yellow'
-      }).start()
+      }).start();
 
       try {
         // Navigate to trade
-        await this.page.goto(tradeUrl)
+        await this.page.goto(tradeUrl);
 
         // Edit trade
-        await this.page.click("[href^='/trade/edit']")
-        await this.page.waitForNavigation()
+        await this.page.click("[href^='/trade/edit']");
+        await this.page.waitForNavigation();
 
         // Save
-        await this.page.click('.rlg-btn-primary[type="submit"]')
-        await this.page.waitForNavigation()
+        await this.page.click('.rlg-btn-primary[type="submit"]');
+        await this.page.waitForNavigation();
 
         if (this.target === 'all') {
           // Wait for bump cooldown between bumps
-          await this.delay(1000 * 10)
+          await this.delay(1000 * 10);
         }
 
-        const secondsElapsed = moment().diff(start, 'seconds')
-        spinner.succeed(this.target === 'oldest'
-          ? `Bumped oldest active trade ${chalk.dim(`(${secondsElapsed} seconds)`)}`
-          : `Bumped trade ${humanIndex}/${tradeUrls.length} ${chalk.dim(`(${secondsElapsed} seconds)`)}`
-        )
+        const secondsElapsed = moment().diff(start, 'seconds');
+        spinner.succeed(
+          this.target === 'oldest'
+            ? `Bumped oldest active trade ${chalk.dim(
+                `(${secondsElapsed} seconds)`
+              )}`
+            : `Bumped trade ${humanIndex}/${tradeUrls.length} ${chalk.dim(
+                `(${secondsElapsed} seconds)`
+              )}`
+        );
       } catch (error) {
-        const secondsElapsed = moment().diff(start, 'seconds')
-        spinner.fail(this.target === 'oldest'
-          ? `Failed to bump oldest active trade ${chalk.dim(`(${secondsElapsed} seconds)`)}`
-          : `Failed to bump trade ${humanIndex}/${tradeUrls.length} ${chalk.dim(`(${secondsElapsed} seconds)`)}`
-        )
+        const secondsElapsed = moment().diff(start, 'seconds');
+        spinner.fail(
+          this.target === 'oldest'
+            ? `Failed to bump oldest active trade ${chalk.dim(
+                `(${secondsElapsed} seconds)`
+              )}`
+            : `Failed to bump trade ${humanIndex}/${
+                tradeUrls.length
+              } ${chalk.dim(`(${secondsElapsed} seconds)`)}`
+        );
       }
     }
   }
@@ -170,16 +201,18 @@ class Dodgem {
   /**
    * Delay the specified interval and then begin bumping again
    */
-  async delayBumpTrades () {
-    const minutes = 1000 * 60 * this.interval
-    const nextRunTs = moment().add(this.interval, 'minutes').format('HH:mm:ss')
+  async delayBumpTrades() {
+    const minutes = 1000 * 60 * this.interval;
+    const nextRunTs = moment()
+      .add(this.interval, 'minutes')
+      .format('HH:mm:ss');
 
-    new Ora(`Dodgem will run again at: ${chalk.green(nextRunTs)}`).info()
+    new Ora(`Dodgem will run again at: ${chalk.green(nextRunTs)}`).info();
 
-    await this.delay(minutes)
-    const tradeUrls = await this.scrapeTrades()
-    await this.bumpTrades(tradeUrls)
-    await this.delayBumpTrades()
+    await this.delay(minutes);
+    const tradeUrls = await this.scrapeTrades();
+    await this.bumpTrades(tradeUrls);
+    await this.delayBumpTrades();
   }
 
   /**
@@ -188,10 +221,13 @@ class Dodgem {
    * @param {Number} timeout
    * @returns {Promise}
    */
-  async delay (timeout) {
-    return new Promise(resolve => { setTimeout(() => { resolve() }, timeout) })
+  async delay(timeout) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, timeout);
+    });
   }
-
 }
 
-module.exports = Dodgem
+module.exports = Dodgem;
--- src/app.js
+++ src/app.js
@@ -1,49 +1,46 @@
 #!/usr/bin/env node
-'use strict'
 
-// Project Dependencies
-const app = require('caporal')
-const Ora = require('ora')
-const inquirer = require('inquirer')
-const Preferences = require('preferences')
+const app = require('caporal');
+const Ora = require('ora');
+const pjson = require('../package.json');
+const Dodgem = require('./Dodgem');
+const inquirer = require('inquirer');
+const Preferences = require('preferences');
 
-const Dodgem = require('./Dodgem')
-const pjson = require('../package.json')
-const credentials = new Preferences("com.jamieweavis.dodgem");
+const credentials = new Preferences('com.jamieweavis.dodgem');
 
-/**
- * Start command
- */
-async function start () {
-  if (!credentials.emailAddress || !credentials.password) {
-    await login()
-  }
-  const dodgem = new Dodgem(credentials)
-  await dodgem.init()
-  await dodgem.login()
-  const tradeUrls = await dodgem.scrapeTrades()
-  await dodgem.bumpTrades(tradeUrls)
-  await dodgem.delayBumpTrades()
+async function login() {
+  new Ora('Please enter login credentials for Rocket League Garage').info();
+  await inquirer
+    .prompt([
+      {
+        name: 'emailAddress',
+        message: 'Email Address:'
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Password:'
+      }
+    ])
+    .then(async data => {
+      credentials.emailAddress = data.emailAddress;
+      credentials.password = data.password;
+      credentials.save();
+      new Ora('Rocket League Garage login credentials saved').succeed();
+    });
 }
 
-/**
- * Login command
- */
-async function login () {
-  new Ora('Please enter login credentials for Rocket League Garage').info()
-  await inquirer.prompt([{
-    name: 'emailAddress',
-    message: 'Email Address:'
-  }, {
-    type: 'password',
-    name: 'password',
-    message: 'Password:'
-  }]).then(async data => {
-    credentials.emailAddress = data.emailAddress
-    credentials.password = data.password
-    credentials.save()
-    new Ora('Rocket League Garage login credentials saved').succeed()
-  })
+async function start() {
+  if (!credentials.emailAddress || !credentials.password) {
+    await login();
+  }
+  const dodgem = new Dodgem(credentials);
+  await dodgem.init();
+  await dodgem.login();
+  const tradeUrls = await dodgem.scrapeTrades();
+  await dodgem.bumpTrades(tradeUrls);
+  await dodgem.delayBumpTrades();
 }
 
 app
@@ -56,6 +53,6 @@ app
 
   // Login command
   .command('login', 'Set login credentials for Rocket League Garage')
-  .action(login)
+  .action(login);
 
-app.parse(process.argv)
+app.parse(process.argv);
